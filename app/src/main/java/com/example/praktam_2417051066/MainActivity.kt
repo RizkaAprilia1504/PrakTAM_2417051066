554847acde8ff347e2bb1774fb5a0e5021e6a48e
Rizka Aprilia <rizkaaprilia567@gmail.com>
2026-03-14 11:42:14 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam_2417051066/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051066/MainActivity.kt
@@ -6,28 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import com.example.praktam_2417051066.model.Product
 import com.example.praktam_2417051066.model.ProductSource
+import com.example.praktam_2417051066.ui.theme.PrakTAM_2417051066Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
-            DaftarProdukScreen()
+            PrakTAM_2417051066Theme {
+                DaftarProdukScreen()
+            }
         }
     }
 }
@@ -56,6 +64,8 @@ fun DaftarProdukScreen() {
 @Composable
 fun DetailScreen(product: Product) {
 
+    var isFavorite by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier.fillMaxWidth()
     ) {
@@ -64,13 +74,34 @@ fun DetailScreen(product: Product) {
             modifier = Modifier.padding(16.dp)
         ) {
 
-            Image(
-                painter = painterResource(id = product.imageRes),
-                contentDescription = product.nama,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(180.dp)
-            )
+            Box {
+
+                Image(
+                    painter = painterResource(id = product.imageRes),
+                    contentDescription = product.nama,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(180.dp)
+                )
+
+                IconButton(
+                    onClick = { isFavorite = !isFavorite },
+                    modifier = Modifier.align(Alignment.TopEnd)
+                ) {
+
+                    Icon(
+                        imageVector =
+                            if (isFavorite) Icons.Filled.Favorite
+                            else Icons.Outlined.FavoriteBorder,
+
+                        contentDescription = "Favorite",
+
+                        tint =
+                            if (isFavorite) Color.Red
+                            else Color.White
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.height(8.dp))
 
@@ -99,5 +130,7 @@ fun DetailScreen(product: Product) {
 @Preview(showBackground = true)
 @Composable
 fun DaftarProdukPreview() {
-    DaftarProdukScreen()
+    PrakTAM_2417051066Theme {
+        DaftarProdukScreen()
+    }
 }
